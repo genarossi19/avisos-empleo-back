3df046ed2ad5ff4b5820cba8fc85aee75078f1d6
genarossi19 <genaros.gr@gmail.com>
2025-09-23 12:41:03 -0300

    primeros get de la api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import logger from "morgan";
-
+import cors from "cors";
 dotenv.config();
+
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(logger("dev"));
 const port = process.env.PORT;
 
@@ -13,6 +15,45 @@ app.get("/admin", (req, res) => {
 
 app.get("/", (_, res) => {
   res.send(`Backend corriendo en puerto: ${port}`);
+});
+
+app.get("/company", (_, res) => {
+  res.json({
+    name: "Coderhouse",
+    email: "0oNt8@example.com",
+    location: "Buenos Aires",
+  });
+});
+
+const fechaActual = new Date();
+
+app.get("/jobs-postings", (_, res) => {
+  res.json([
+    {
+      id: 1,
+      title: "Frontend Developer",
+      description: "Desarrollo de software en frontend react",
+      location: "Buenos Aires",
+      contractType: "Full Time",
+      publicationDate: fechaActual,
+    },
+    {
+      id: 2,
+      title: "Backend Developer",
+      description: "Desarrollo de backend en express",
+      location: "Trenque Lauquen",
+      contractType: "Full Time",
+      publicationDate: fechaActual,
+    },
+    {
+      id: 3,
+      title: "BD Developer",
+      description: "Desarrollo de base de datos en sql server",
+      location: "La Plata",
+      contractType: "Full Time",
+      publicationDate: fechaActual,
+    },
+  ]);
 });
 
 app.listen(port, () => {
